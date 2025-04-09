83697b53875e8bbabd72ac6c84e30e857711f812
Shayan Sen <senshayan2103@gmail.com>
2025-04-09 14:12:04 +0530

    commit 02

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:final_year_codechamps_2/pages/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main()async{
+Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
