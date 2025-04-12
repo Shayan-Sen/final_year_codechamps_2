69220759bbadbab1426866667a53502d0d98186c
Shayan Sen <senshayan2103@gmail.com>
2025-04-12 15:51:25 +0530

    Added account deletion and password update from current password

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -5,25 +5,63 @@ class AuthServices {
   User? get currentUser => auth.currentUser;
   Stream<User?> get authStateChanges => auth.authStateChanges();
 
-  Future<UserCredential> createAccount({required String email,required String password})async{
-    return await auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<UserCredential> createAccount({
+    required String email,
+    required String password,
+  }) async {
+    return await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
-  Future<UserCredential> signIn({required String email,required String password})async{
-    return await auth.signInWithEmailAndPassword(email: email, password: password);
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
-  Future<void> signOut()async{
+  Future<void> signOut() async {
     await auth.signOut();
   }
 
-  Future<void> resetPassword({required String email})async{
+  Future<void> resetPassword({required String email}) async {
     await auth.sendPasswordResetEmail(email: email);
   }
 
-  Future<void> updateUsername({required String username})async{
-    if(currentUser!=null){
+  Future<void> updateUsername({required String username}) async {
+    if (currentUser != null) {
       await currentUser!.updateDisplayName(username);
     }
+  }
+
+  Future<void> deleteAccount({
+    required String username,
+    required String password,
+  }) async {
+    AuthCredential credential = EmailAuthProvider.credential(
+      email: username,
+      password: password,
+    );
+    await currentUser!.reauthenticateWithCredential(credential);
+    await currentUser!.delete();
+    await auth.signOut();
+  }
+
+  Future<void> updatePasswordFromCurrentPassword({
+    required String currentPassword,
+    required String newPassword,
+    required String email,
+  }) async {
+    AuthCredential credential = EmailAuthProvider.credential(
+      email: email,
+      password: currentPassword,
+    );
+    await currentUser!.reauthenticateWithCredential(credential);
+    await currentUser!.updatePassword(newPassword);
   }
 }
