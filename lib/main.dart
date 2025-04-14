e41ae0fd89ef186eeaaea22aeffb6cc2f211578e
Shayan Sen <senshayan2103@gmail.com>
2025-04-14 23:36:24 +0530

    Implemented FirebaseAuth state change listener to navigate to HomePage upon successful login.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:final_year_codechamps_2/firebase_options.dart';
+import 'package:final_year_codechamps_2/pages/homepage.dart';
 import 'package:final_year_codechamps_2/pages/loginpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +16,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      title: "Smart Tutor",
+    );
+  }
+}
+
+class DefaultPage extends StatelessWidget {
+  const DefaultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Something went wrong: ${snapshot.error}'),
+            );
+          }
+        }
+        return LoginPage();
+      },
+    );
   }
 }
 
