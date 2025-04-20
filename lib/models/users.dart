1563001a9e34ec37b87b80b817dbf495ae11007f
Shayan Sen <senshayan2103@gmail.com>
2025-04-20 17:17:49 +0530

    Updated the Users model to use a String for the profile image instead of DocumentSnapshot and defined the Roles enum.

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,11 +1,14 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+enum Roles { teacher, student }
 
-enum Roles{teacher,student}
-
-class Users{
-  Users({required this.name,required this.email,required this.role,required this.profileImage});
+class Users {
+  Users({
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.profileImage,
+  });
   final String name;
   final String email;
-  final DocumentSnapshot profileImage;
+  final String profileImage;
   final Roles role;
 }
