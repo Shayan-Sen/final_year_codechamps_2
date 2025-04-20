81eab54b269889e7a280a1c75171ffc182f8eb54
Shayan Sen <senshayan2103@gmail.com>
2025-04-20 17:34:29 +0530

    feat: Add storage file model and update user model
    Added a new model class `StorageFile` to represent files with download URL, name, and extension. Updated the `Users` model to include a `profileImage` field as a map of dynamic values and added `fromFirestore` and `toFirestore` methods for Firestore data conversion.

---
+++ lib/models/storage_file.dart
@@ -0,0 +1,6 @@
+class StorageFile{
+  StorageFile({required this.downloadUrl, required this.name, required this.extension});
+  final String downloadUrl;
+  final String name;
+  final String extension;
+}
--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 enum Roles { teacher, student }
 
 class Users {
@@ -9,6 +11,25 @@ class Users {
   });
   final String name;
   final String email;
-  final String profileImage;
+  final Map<String,dynamic> profileImage;
   final Roles role;
+
+  factory Users.fromFirestore(DocumentSnapshot<Map<String,dynamic>> snapshot) {
+    final data = snapshot.data();
+    return Users(
+      name: data!['name'],
+      email: data['email'],
+      role: Roles.values.firstWhere((role) => role.name == data['role']),
+      profileImage: data['profileImage'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'email': email,
+      'role': role.name,
+      'profileImage': profileImage,
+    };
+  }
 }
